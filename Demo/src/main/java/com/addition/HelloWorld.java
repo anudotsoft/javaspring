422396045c1b98c75e6c52a06d4a7a887f13b1a5
Dell <anudotsoft@gmail.com>
2022-06-16 18:45:28 +0530

    This is Hello world Program

---
+++ Demo/src/main/java/com/addition/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.addition;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("helloworld code");
+		System.out.println("I am from india");
+		System.out.println("Sravanthi");
+		System.out.println("Sai Deepika");
+		System.out.println("Megana");
+		System.out.println("Palanikumar");
+		System.out.println("Afrin fathima");
+		System.out.println("Ritika");
+		
+
+	}
+
+}
