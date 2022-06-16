a25d367b0efc8f6b851985d3bdaaf5c8247ed19f
Dell <anudotsoft@gmail.com>
2022-06-16 18:32:19 +0530

    First Program Upload to GitHub

---
+++ Demo/src/main/java/com/addition/Add.java
@@ -0,0 +1,14 @@
+package com.addition;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=30;
+		c=a+b;
+		System.out.println("addition is :\t"+c);
+
+	}
+
+}
